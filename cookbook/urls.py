f3b8487f27c9bdec409f6f61ce3535943432e54d
julielhote <julie.lhote@firstroot.co>
2021-01-15 10:43:41 +0100

    changes in urlpatterns

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -23,5 +23,5 @@ from cookbook.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
